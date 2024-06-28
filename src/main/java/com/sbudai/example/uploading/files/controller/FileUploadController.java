e1ef52b8887c7956e6985b4670118cde3e0a36d6
pingvin65 <12439378+pingvin65@users.noreply.github.com>
2024-06-27 23:23:00 -0400

    update

--- src/main/java/com/sbudai/example/uploading/files/controller/FileUploadController.java
+++ src/main/java/com/sbudai/example/uploading/files/controller/FileUploadController.java
@@ -12,52 +12,69 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+/**
+ * 
+ */
 @Controller
 public class FileUploadController {
-	
-private static final Logger logger = LoggerFactory.getLogger(FileUploadController.class);
 
-	
-	public static String uploadDirectory = System.getProperty("user.dir") + File.separator +"uploads";
+	private static final Logger logger = LoggerFactory.getLogger(FileUploadController.class);
 
-	
+	public static String uploadDirectory = System.getProperty("user.dir") + File.separator + "uploads";
 
-	@RequestMapping("/")
+	/**
+	 * 
+	 * @param model Model
+	 * @return String
+	 * @throws IOException
+	 */
+	@GetMapping("/")
 	public String home(Model model) throws IOException {
 		File dir = new File("uploads");
 
 		logger.info("Getting all files in " + dir.getCanonicalPath() + " including those in subdirectories");
 		List<File> files = (List<File>) FileUtils.listFiles(dir, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
 		for (File file : files) {
-			logger.info("file: " + file.getCanonicalPath() + " " + FileUtils.byteCountToDisplaySize(FileUtils.sizeOf(file)));
+			logger.info("file: " + file.getCanonicalPath() + " "
+					+ FileUtils.byteCountToDisplaySize(FileUtils.sizeOf(file)));
 
 		}
+
 		model.addAttribute("files", files);
 
 		return "uploadview";
 	}
 
-	@RequestMapping("/upload")
+	/**
+	 * 
+	 * @param model              Model
+	 * @param multipartFile      MultipartFile
+	 * @param redirectAttributes RedirectAttributes
+	 * @return String
+	 * @throws IOException
+	 */
+	@PostMapping("/upload")
 	public String upload(Model model, @RequestParam("files") MultipartFile multipartFile,
 			RedirectAttributes redirectAttributes) throws IOException {
-
-		if(multipartFile.isEmpty()) {
-			redirectAttributes.addFlashAttribute("warning",
-					"There is no file to upload !");
+		logger.error("------ {}", multipartFile.getBytes().length);
+		if (multipartFile.isEmpty()) {
+			redirectAttributes.addFlashAttribute("warning", "There is no file to upload !");
 			return "redirect:/";
 		}
-		
+
 		InputStream fileStream = multipartFile.getInputStream();
 		File targetFile = new File(
 				uploadDirectory + System.getProperty("file.separator") + multipartFile.getOriginalFilename());
 		FileUtils.copyInputStreamToFile(fileStream, targetFile);
-		redirectAttributes.addFlashAttribute("message",
-				"You successfully uploaded " + targetFile.getName() + "!");
+		redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + targetFile.getName() + "!");
 		model.addAttribute("message", "Successfully uploaded files ");
 		return "redirect:/";
 	}
